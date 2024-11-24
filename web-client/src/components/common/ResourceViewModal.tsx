4c66606e6b4fa0615de988ce68ad7aaee5bee2b4
merlijn <vanittersum@gmail.com>
2024-11-24 12:38:35 +0100

    remove test code

--- web-client/src/components/common/ResourceViewModal.tsx
+++ web-client/src/components/common/ResourceViewModal.tsx
@@ -4,18 +4,9 @@ import {isMobile} from "react-device-detect";
 import {boundedRatioBox} from "../../api/Util";
 import './ResourceViewModal.css';
 import Modal from "./Modal";
-import {
-  isHLSProvider,
-  MediaPlayer,
-  MediaPlayerInstance, MediaProvider,
-  MediaProviderAdapter,
-  MediaProviderChangeEvent,
-} from "@vidstack/react";
+import {MediaPlayer, MediaPlayerInstance, MediaProvider,} from "@vidstack/react";
 
-import {
-  defaultLayoutIcons,
-  DefaultVideoLayout,
-} from '@vidstack/react/player/layouts/default';
+import {defaultLayoutIcons, DefaultVideoLayout,} from '@vidstack/react/player/layouts/default';
 
 const ResourceViewModal = (props: { resource?: Resource, onHide: () => void }) => {
 
@@ -27,14 +18,8 @@ const ResourceViewModal = (props: { resource?: Resource, onHide: () => void }) =
     setSrc(props.resource?.urls.originalResourceUrl || '')
   }, [props.resource]);
 
-  useEffect(() => {
-    console.log('player', player.current?.title)
-  }, [player])
-
   const modalSize = (v: Resource): CSSProperties => {
-
-    const w = isMobile ? "100vw" : "75vw"
-    return boundedRatioBox(w, "75vh", v.resourceMeta.width / v.resourceMeta.height)
+    return boundedRatioBox(isMobile ? "100vw" : "75vw", "75vh", v.resourceMeta.width / v.resourceMeta.height)
   }
 
   let isVideo = props.resource?.contentType.startsWith("video") || false
@@ -46,14 +31,9 @@ const ResourceViewModal = (props: { resource?: Resource, onHide: () => void }) =
     props.onHide()
   }
 
-  const handleDoubleClick: MouseEventHandler<HTMLElement> = (event) => {
-    event.preventDefault();
-    event.stopPropagation(); // Stops the event from triggering any default or library behaviors
-  };
-
   return (
       <Modal visible = { props.resource !== undefined } onHide = { onHide }>
-        <div className="video-modal-content" style = { props.resource && modalSize(props.resource)} onDoubleClick = { handleDoubleClick }>
+        <div className="video-modal-content" style = { props.resource && modalSize(props.resource)}>
           <MediaPlayer
             className = "player"
             tab-index = '-1'
@@ -64,7 +44,6 @@ const ResourceViewModal = (props: { resource?: Resource, onHide: () => void }) =
             style = { !isVideo ? { display: "none" } : {} }
             controlsDelay = { 5000 }
             hideControlsOnMouseLeave = { true }
-            onDoubleClick = { handleDoubleClick }
             // keep-alive
             // logLevel = "debug"
             autoPlay = { true }
@@ -72,11 +51,8 @@ const ResourceViewModal = (props: { resource?: Resource, onHide: () => void }) =
             // onCanPlay = { autoPlay }
             // onProviderChange = { onProviderChange }
           >
-            <MediaProvider onDoubleClick={handleDoubleClick} />
-            <DefaultVideoLayout
-                icons = { defaultLayoutIcons }
-                onDoubleClick={handleDoubleClick}
-            />
+            <MediaProvider />
+            <DefaultVideoLayout icons = { defaultLayoutIcons } />
           </MediaPlayer>
           { isImage &&  <img style = {{ width: "100%", height: "100%", visibility : isImage ? "visible" : "hidden" }} src = { src }/> }
         </div>
