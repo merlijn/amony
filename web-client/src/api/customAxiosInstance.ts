e2a2568470f97dea359bafabf90d6e9ace14ceea
merlijn <merlijn.van_ittersum@dhl.com>
2025-01-03 16:24:22 +0100

    add refresh token in custom axios

--- web-client/src/api/customAxiosInstance.ts
+++ web-client/src/api/customAxiosInstance.ts
@@ -1,22 +1,58 @@
-import axios, {AxiosRequestConfig} from "axios";
+import axios, {AxiosError, AxiosRequestConfig} from "axios";
 
-export const customAxiosInstance = <T>(config: AxiosRequestConfig, options?: AxiosRequestConfig): Promise<T> => {
-  const source = axios.CancelToken.source()
-  const promise = axios({
-    ...config,
-    ...options,
-    cancelToken: source.token,
-  })
-    .then(({ data }) => data)
-    .catch(error => {
-
-      return null
-    })
-
-  // @ts-ignore
-  promise.cancel = () => {
-    source.cancel('Query was cancelled')
-  }
-
-  return promise
+async function refreshToken() {
+  const response = await axios.post('/api/auth/refresh');
+  // Update token storage here if needed
+  return response;
 }
+
+export const customAxiosInstance = <T>(
+  config: AxiosRequestConfig,
+  options?: AxiosRequestConfig
+): Promise<T> => {
+  const source = axios.CancelToken.source();
+
+  const executeRequest = (
+    config: AxiosRequestConfig,
+    options?: AxiosRequestConfig,
+    refreshTokenOn401: boolean = true
+  ): Promise<T> => {
+    // Update authorization header with current token
+    const currentConfig = {
+      ...config,
+      // headers: {
+      //   ...config.headers,
+      //   Authorization: `Bearer ${getCurrentToken()}`, // Implement getCurrentToken()
+      // },
+    };
+
+    const promise = axios({
+      ...currentConfig,
+      ...options,
+      cancelToken: source.token,
+    })
+      .then(({ data }) => data)
+      .catch(async (error: AxiosError) => {
+        if (error.response?.status === 401 && refreshTokenOn401) {
+          try {
+            const refreshResponse = await refreshToken();
+            if (refreshResponse.status === 200) {
+              return executeRequest(config, options, false);
+            }
+          } catch (refreshError) {
+            throw error;
+          }
+        }
+        throw error;
+      });
+
+    // @ts-ignore
+    promise.cancel = () => {
+      source.cancel('Query was cancelled');
+    };
+
+    return promise;
+  };
+
+  return executeRequest(config, options);
+};
