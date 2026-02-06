0ea0a11171e0b47551d08e54a11abd5b272576f6
merlijn <merlijn.van_ittersum@dhl.com>
2026-02-06 14:56:17 +0100

    fixes

--- backend/src/main/resources/db/03-auth.sql
+++ backend/src/main/resources/db/03-auth.sql
@@ -4,6 +4,6 @@ CREATE TABLE users (
     auth_subject     VARCHAR(64) NOT NULL,
     email            VARCHAR(64) NOT NULL UNIQUE,
     time_registered  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    roles[]          VARCHAR(256) NOT NULL
-    CONSTRAINT unique_user_auth UNIQUE (oauth_provider, oauth_subject)
+    roles            VARCHAR(256)[] NOT NULL,
+    CONSTRAINT unique_user_auth UNIQUE (auth_provider, auth_subject)
 );
--- backend/src/main/scala/nl/amony/modules/auth/dal/UserDatabase.scala
+++ backend/src/main/scala/nl/amony/modules/auth/dal/UserDatabase.scala
@@ -13,15 +13,16 @@ class UserDatabase(pool: Resource[IO, Session[IO]]) extends Logging:
   private def useTransaction[A](f: (Session[IO], Transaction[IO]) => IO[A]): IO[A] = pool.use(s => s.transaction.use(tx => f(s, tx)))
 
   val getByIdQuery: skunk.Query[String, UserRow] =
-    sql"SELECT id, email, oauth_provider, oauth_subject FROM users WHERE id = $varchar(64)".query(UserRow.decoder)
+    sql"SELECT id, email, auth_provider, auth_subject, time_registered, roles FROM users WHERE id = ${varchar(64)}".query(UserRow.decoder)
 
   val getByEmailQuery: skunk.Query[String, UserRow] =
-    sql"SELECT id, email, oauth_provider, oauth_subject FROM users WHERE email = $varchar(64)".query(UserRow.decoder)
+    sql"SELECT id, email, auth_provider, auth_subject, time_registered, roles FROM users WHERE email = ${varchar(64)}".query(UserRow.decoder)
 
   val insertQuery: skunk.Command[UserRow] =
-    sql"INSERT INTO users (id, email, oauth_provider, oauth_subject) VALUES ($varchar(64), $varchar(64), $varchar(64), $varchar(64), $timestamptz, $_varchar)".command.to[
-      UserRow
-    ]
+    sql"""
+      INSERT INTO users (id, email, auth_provider, auth_subject, time_registered, roles)
+      VALUES (${varchar(64)}, ${varchar(64)}, ${varchar(64)}, ${varchar(64)}, $timestamptz, $_varchar)
+    """.command.to[UserRow]
 
   def getById(userId: String): IO[Option[UserRow]] =
     pool.use(_.prepare(getByIdQuery).flatMap(_.option(userId)))
