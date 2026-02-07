e6d36a823432ebca1da0844d7983e7c419cc0928
merlijn <vanittersum@gmail.com>
2026-02-07 18:15:03 +0100

    dex domain

---
+++ docker/dex/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+# Substitute only our named variables, leaving bcrypt $2a$... hashes intact
+envsubst '${DOMAIN} ${DEX_ADMIN_EMAIL} ${DEX_ADMIN_USERNAME} ${DEX_ADMIN_PASSWORD_HASH}' \
+  < /etc/dex/config.yaml.template > /tmp/config.yaml
+
+exec dex serve /tmp/config.yaml
